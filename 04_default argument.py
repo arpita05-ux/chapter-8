68d0840dc9c7cfcc0319593c385906bb2af609b7
“Arpita <arpitakumari10653@gmail.com>
2025-06-14 10:16:58 +0530

    using default argument

---
+++ 04_default argument.py
@@ -0,0 +1,10 @@
+def goodday(name, ending="thank you"):
+    print("Good day," + name)
+    print(ending)
+
+goodday("Arpita", "Thanks")  
+   
+goodday("krish") 
+
+
+# in this default arugment if i give ending then it take that cmd if i can't then by default it take thank you.
